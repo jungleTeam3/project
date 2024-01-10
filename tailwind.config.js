dbc9b038e6aceeb324b316d075508aa0f307a545
younggun339 <younggun0816@gmail.com>
2024-01-11 08:32:50 +0900

    현재진행목록

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./project/templates/*.html", "./project/static/css/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
