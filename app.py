a6e499102f2c1629ae945f0ec7d9b2452c8b9937
skdddfhg123 <skdddfhg6@gmail.com>
2024-01-10 15:33:27 +0900

    map 수정

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['team_3']
+collection = db['restaurant']  # 컬렉션 이름을 정확히 맞춥니다.
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route('/render')
-def render():
-   return render_template('main.html')
-
-@app.route('/update_content')
-def update_content():
-   # 실제로는 여기에서 필요한 데이터를 가공하여 반환
-   new_content = '<p>Updated content from the server!</p>'
-   return new_content
-
 @app.route('/map')
 def map():
    return render_template('test.html')
 
-if __name__ == '__main__':
+@app.route('/api/list', methods=['GET'])
+def stars_list():
+   # 1. db에서 mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
+   # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
+   stars = list(db.restaurant.find({}, {'_id': False}).sort('name', -1))
+   # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
+   return jsonify({'result': 'success', 'stars_list': stars})
+
+
+#종류 별로 하나씩
+
+
+if __name__ == '__main__':  
    app.run('0.0.0.0',port=5001,debug=True)
